e2e3052a8f6b8bbbf8a6ffec32b4436775049546
Asel Tabaldieva <asel@Asels-MacBook-Pro.local>
2020-03-12 20:59:35 -0500

    Changed file

--- src/test/java/Utils/ConfigReader.java
+++ src/test/java/Utils/ConfigReader.java
@@ -11,6 +11,7 @@ public class ConfigReader {
         String path="configration.properties";
         try {
             FileInputStream input = new FileInputStream(path);
+            
             configFile=new Properties();
             configFile.load(input);
             input.close();
